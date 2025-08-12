973a07879a506c14cbf1ec2c712187391eafc6dd
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-08-12 18:23:40 +0800

    code

--- vagen/env/__init__.py
+++ vagen/env/__init__.py
@@ -1,20 +1,20 @@
 # First, import the modules that are assumed to be always available
-from .sokoban import SokobanEnv, SokobanEnvConfig, SokobanService, SokobanServiceConfig
-from .frozenlake import FrozenLakeEnv, FrozenLakeEnvConfig, FrozenLakeService, FrozenLakeServiceConfig
+# from .sokoban import SokobanEnv, SokobanEnvConfig, SokobanService, SokobanServiceConfig
+# from .frozenlake import FrozenLakeEnv, FrozenLakeEnvConfig, FrozenLakeService, FrozenLakeServiceConfig
 
 REGISTERED_ENV = {
-    "sokoban": {
-        "env_cls": SokobanEnv,
-        "config_cls": SokobanEnvConfig,
-        "service_cls": SokobanService,
-        "service_config_cls": SokobanServiceConfig
-    },
-    "frozenlake": {
-        "env_cls": FrozenLakeEnv,
-        "config_cls": FrozenLakeEnvConfig,
-        "service_cls": FrozenLakeService,
-        "service_config_cls": FrozenLakeServiceConfig
-    }
+    # "sokoban": {
+    #     "env_cls": SokobanEnv,
+    #     "config_cls": SokobanEnvConfig,
+    #     "service_cls": SokobanService,
+    #     "service_config_cls": SokobanServiceConfig
+    # },
+    # "frozenlake": {
+    #     "env_cls": FrozenLakeEnv,
+    #     "config_cls": FrozenLakeEnvConfig,
+    #     "service_cls": FrozenLakeService,
+    #     "service_config_cls": FrozenLakeServiceConfig
+    # }
 }
 
 try:
@@ -73,3 +73,13 @@ try:
 except ImportError:
     pass
 
+try:
+    from vagen.env.spati import SpatiEnv, SpatiEnvConfig, SpatiService, SpatiServiceConfig
+    REGISTERED_ENV["spati"] = {
+        "env_cls": SpatiEnv,
+        "config_cls": SpatiEnvConfig,
+        "service_cls": SpatiService,
+        "service_config_cls": SpatiServiceConfig
+    }
+except ImportError:
+    pass
