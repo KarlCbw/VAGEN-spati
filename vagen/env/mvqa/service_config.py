6546645d2bebe60a02731a886e1bc5805e220780
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-08-12 17:00:46 +0800

    env

---
+++ vagen/env/mvqa/service_config.py
@@ -0,0 +1,8 @@
+from vagen.env.base.base_service_config import BaseServiceConfig
+from dataclasses import dataclass
+
+@dataclass
+class EmbodiedVstarServiceConfig(BaseServiceConfig):
+    use_state_reward: bool = False
+    max_workers: int = 10
+
